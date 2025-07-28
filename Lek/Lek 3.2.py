4672211c0198f19ea3e0cb6b01130675bda44765
Alerie-Ler <zinchenko.darya@gmail.com>
2025-07-28 16:51:13 +0200

    Create Lek 3.2.py

---
+++ Lek/Lek 3.2.py
@@ -0,0 +1,24 @@
+'''
+Napisz program, który:
+
+    wczyta od użytkownika dowolny tekst,
+    usunie z niego spacje i znaki interpunkcyjne,
+    policzy, ile razy występuje każda litera (bez rozróżniania wielkości liter),
+    wypisze wynik w formie:
+
+    a: 3
+    b: 1
+    c: 5
+'''
+
+texst=input("Podaj dowolny takst")
+print(texst.strip().strip(" ,.:;"))
+
+licznik = {}
+for litera in tekst
+    if litera.isalpha():
+        if litera in licznik:
+            licznik[litera]+=1
+        else:
+            licznik[litera]=1
+print(licznik)
